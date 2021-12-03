cb4d63a1c4ff80e17eec6705035bef9577263796
Renan Cunha <renan2610@gmail.com>
2021-12-02 22:22:16 -0300

    Section 3 - Persistence and Data Access with Spring Data JPA

---
+++ Section 3 - Persistence and Data Access with Spring Data JPA/vendas/src/main/java/io/github/renanhcunha/VendasApplication.java
@@ -0,0 +1,58 @@
+package io.github.renanhcunha;
+
+import io.github.renanhcunha.domain.entity.Cliente;
+import io.github.renanhcunha.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes) {
+        return args -> {
+            System.out.println("Salvando clientes");
+            clientes.save(new Cliente("Douglas"));
+            clientes.save(new Cliente("Outro Cliente"));
+
+            List<Cliente> todosClientes = clientes.findAll();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Atualizando clientes");
+            todosClientes.forEach(c -> {
+                c.setNome(c.getNome() + " atualizado.");
+                clientes.save(c);
+            });
+
+            todosClientes = clientes.findAll();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Buscando clientes");
+            clientes.findByNomeLike("Cli").forEach(System.out::println);
+
+            System.out.println("Deletando clientes");
+            clientes.findAll().forEach(c -> {
+                clientes.delete(c);
+            });
+
+            todosClientes = clientes.findAll();
+
+            if(todosClientes.isEmpty()) {
+                System.out.println("Nenhum cliente encontrado");
+            } else {
+                todosClientes.forEach(System.out::println);
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
+}
